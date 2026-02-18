ca2ee243b789432e844604eeec531dc90e2d3b1c
dwnamit105-png <dwnamit105@gmail.com>
2026-02-19 00:36:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1329,8 +1329,8 @@ process.on('SIGINT', () => {
     process.exit();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 
